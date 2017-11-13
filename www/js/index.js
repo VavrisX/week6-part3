f6a793c6d729528898637ecb087cf09ea4332b6e
VavrisX <vavl1_16@uni.worc.ac.uk>
2017-11-13 17:27:37 +0000

    x

--- www/js/index.js
+++ www/js/index.js
@@ -1,26 +1,28 @@
-var formArr;    
+   
 
 function submitForm() {
-	var text = $('#textinput').val();
-    var name = ...
-    var email = ...
     
     
-    var jsonObject = { "text" : text, "name" : name, ..}
+    var newUser = new Object();
+	
+     newUser.name = $('#nameInput').val();
+     newUser.email = $('#emailInput').val();
+     newUser.tn = $('#tnInput').val();
+     newUser.dob = $('#dobInput').val();
+     newUser.password = $('#passwordInput').val();
     
-
-	alert(text);
-    storeValue("forms", jsonObject);
+    
+ if(sessionStorage.user)
+    {
+     user= JSON.parse(sessionStorage.getItem('user'));
+    }else{
+     user=[];
+    }
+ user.push(newUser )   
+    console.log(user);
+    sessionStorage.setItem('user', JSON.stringify(user));
+    
+    var retrievedObject = sessionStorage.getItem('user');
+    console.log('retrievedObject: ', JSON.parse(retrievedObject));
 }
 
-function storeValue(key, value) {
-
-    
-    fomrArr.push(value);
-    
-    
-    localStorage.setItem(key, formArr);
-    
-    
-    
-}
